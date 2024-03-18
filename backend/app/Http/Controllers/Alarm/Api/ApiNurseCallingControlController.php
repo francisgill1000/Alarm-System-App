898577b515240b8649b7b48dc423c7d3bb1050d6
Venu J <149071174+akildevs1004@users.noreply.github.com>
2024-03-18 11:07:36 +0400

    redirect

--- backend/app/Http/Controllers/Alarm/Api/ApiNurseCallingControlController.php
+++ backend/app/Http/Controllers/Alarm/Api/ApiNurseCallingControlController.php
@@ -39,6 +39,39 @@ class ApiNurseCallingControlController extends Controller
 
 
 
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://alarmbackend.xtremeguard.org/api/alarm_device_status',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => '{
+    "serialNumber": "' . $request->serialNumber . '",
+    
+    "alarm_status":1,
+    
+     
+     
+    "time": 147444
+}',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json'
+                ),
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            echo $response;
+
+
+
+
 
             $device_serial_number = $request->serialNumber;
             $switch1_status = $request->switch1_status;
